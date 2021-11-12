c95cc20f7e120852e4c16dc4178368009449c3b2
unknown <eduard.anton2002@gmail.com>
2021-11-12 10:27:30 -0500

    Restructured file and folder access

--- bot.py
+++ bot.py
@@ -1,35 +1,33 @@
 import discord
+import shlex
+import json
 
 intents = discord.Intents.default()
 intents.presences = True
 intents.members = True
 client = discord.Client(intents=intents)
 
-directory = {"Kurius": {"Documents": {"Code": {"Python": {"Bot": {"BotCommands.txt": "commands.txt"} # cmds
-                                                            }
-                                                }, 
-                                    "School": {"Math": {"BigNumber.txt": "bignumber.txt", # Base64
-                                                        "RecursiveAlgo.txt": "algorithm.txt"}, # Algorithm
-                                                "Web Dev": {"homework.html": "index.html"} # Website
-                                                },
-                                    "Work": {}
-                                        },
-                        "Pictures": {"DogPic.png": "dogpic.png", # Steganography
-                                    "CatPic.png": "catpic.png", # Steganography
-                                    "KuriusLogo.png": "logo.png", 
-                                    "Password.png": "password.png"}, # Computer Vision
-                        "Downloads": {}}} # Some cryptic files or something
-
-
 TOKEN = ''
+treeDir = {}
+currentDir = "/"
 
 @client.event
 async def on_ready():
+	global treeDir
     print(f'{client.user} has connected to Discord.')
-
+	treeDir = loadTreeDir() # Load the files and folders
 
 @client.event
 async def on_message(message):
-    global directory
+    global treeDir
+	global currentDir
 
     if message.author == client.user: return # Ignore self messages
+	
+def loadTreeDir():
+	try:
+		with open('director.json', 'r') as file:
+			return json.load(file.read())
+	except:
+		print("Error: Could not read the tree directory")
+		return {}
