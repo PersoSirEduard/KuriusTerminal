be4b437bcfda75af189cdc4a59abe2a8a6879894
Eduard Anton <eduard.anton2002@gmail.com>
2021-11-27 01:55:33 -0500

    Add files via upload

--- BigKurius/bot.py
+++ BigKurius/bot.py
@@ -1,5 +1,7 @@
 import discord
 from flask import Flask, request
+from threading import Thread
+from functools import partial
 import random
 
 # Bot permissions
@@ -10,8 +12,13 @@ client = discord.Client(intents=intents)
 
 app = Flask(__name__)
 
+app_run = partial(app.run, host="0.0.0.0", port=80, debug=True, use_reloader=False)
+
 TOKEN = 'OTE0MDE3MjI2ODc5ODE5Nzc2.YaG64A.GUAjZM8ipMNarx4pOXF0GyJy42U'
 SECURITY_KEY = '[tL1O^Up@9bu}&'
+
+user_passwords = []
+blacklist_users = []
 
 @app.route("/new_message", methods=['POST'])
 def new_message():
@@ -27,8 +34,14 @@ def new_message():
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord.')
-
+	print(f'{client.user} has connected to Discord.')
+	
+@client.event
+async def on_message(message):
+	if message.author.id in blacklist_users:
+		pass
+	
 if __name__ == "__main__":
+	t = Thread(target=app_run)
+	t.start()
 	client.run(TOKEN)
-	app.run(debug=False)
